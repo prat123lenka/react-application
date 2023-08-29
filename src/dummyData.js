26283a641af89f42b79a0e8767fa88d1191612a2
Prativa Lenka <prativalenka124@gmail.com>
2023-08-29 18:33:50 +0530

    first commit

---
+++ src/dummyData.js
@@ -0,0 +1,59 @@
+export const Users = [
+  {
+    id:1,
+    profilePicture: "assets/person/1.jpeg",
+    username: "Prativa ",
+  },
+  {
+    id:2,
+    profilePicture: "assets/person/2.jpeg",
+    username: "Roshni",
+  },
+  {
+    id:3,
+    profilePicture: "assets/person/3.jpeg",
+    username: "Tiyaa",
+  },
+  {
+    id:4,
+    profilePicture: "assets/person/4.jpeg",
+    username: "Shiri",
+  },
+];
+export const Posts = [
+  {
+    id: 1,
+    desc: "Jai Shri Ram.",
+    photo: "assets/post/1.jpeg",
+    date: "5 mins ago",
+    userId: 1,
+    like: 32,
+    comment: 9,
+  },
+  {
+    id: 2,
+    desc: "monsoon",
+    photo: "assets/post/2.jpeg",
+    date: "15 mins ago",
+    userId: 2,
+    like: 2,
+    comment: 1,
+  },
+  {
+    id: 3,
+    desc: "Life is all about replacements",
+    photo: "assets/post/3.jpeg",
+    date: "1 hour ago",
+    userId: 3,
+    like: 61,
+    comment: 2,
+  },
+  {
+    id: 4,
+    photo: "assets/post/4.jpeg",
+    date: "4 hours ago",
+    userId: 4,
+    like: 7,
+    comment: 3,
+  },
+]
